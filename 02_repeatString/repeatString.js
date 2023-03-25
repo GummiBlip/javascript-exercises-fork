35d4ad6ace887e0850e3c5687a9013683c2edee9
GummiBlip <gummiblip@gmail.com>
2023-03-25 15:40:51 -0500

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+  if (repetitions < 0) return "ERROR";
+  let returnString = "";
+  for (let i = 0; i < repetitions; i++) {
+    returnString += string;
+  }
+  return returnString;
 };
 
 // Do not edit below this line
