b747d0cf9e708acae2ebfd3d30d6b62fa477e773
GummiBlip <gummiblip@gmail.com>
2023-03-28 11:47:12 -0500

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...targetValues) {
+  let initialLength = array.length;
+  for (let i = 0; i < initialLength; i++) {
+    while (array.indexOf(targetValues[i]) !== -1) {
+      array.splice(array.indexOf(targetValues[i]), 1);
+  } 
+}
+  return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
