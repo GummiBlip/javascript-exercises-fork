1245b50d110c8f45b186a1b58935f5f2eab7539e
GummiBlip <gummiblip@gmail.com>
2023-04-04 15:08:14 -0500

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(augend, addend) {
+  return augend + addend;
 };
 
-const subtract = function() {
-	
+const subtract = function(subtrahend, minuend) {
+  return subtrahend - minuend;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+  let total = 0;
+  for (number of numberArray) {
+    total += number;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(factorArray) {
+  let product = factorArray[0];
+  for (let i = 0; i < factorArray.length - 1; i++) {
+    product *= factorArray[i + 1];
+  }
+  return product;
+}
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorial = 1;
+  if (number === 0) return factorial;
+  for (let i = 1; i <= number; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
