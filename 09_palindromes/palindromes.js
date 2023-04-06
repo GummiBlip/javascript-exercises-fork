97154bd8c99309854f85f4b8a6645258567c3dc8
GummiBlip <gummiblip@gmail.com>
2023-04-06 11:38:09 -0500

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+  let punctuationRegex = /(\s+|,+|\.+|!+|\?+)/g;
+  let sanitized = string.replace(punctuationRegex, '').toLowerCase()
+  let reversed = sanitized.split('').reverse().join('')
+  if (reversed === sanitized) {
+    return true;
+  }
+  return false;
+}
 // Do not edit below this line
 module.exports = palindromes;
