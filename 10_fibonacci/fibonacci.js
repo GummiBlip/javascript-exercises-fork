9e9da3a534954028ae65313a6ce91db3d3cba26e
GummiBlip <gummiblip@gmail.com>
2023-04-07 12:42:53 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(memberNumber) {
+  if (memberNumber < 1) {
+    return "OOPS";
+  }
+  if (memberNumber == 1 | memberNumber == 2) {
+    return 1;
+  }
+  return fibonacci(memberNumber - 1) + fibonacci(memberNumber - 2);
 };
 
 // Do not edit below this line
